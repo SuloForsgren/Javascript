9c9006206fc4dc19ebbe583a2fbd1287cc8044ee
Your Name <you@example.com>
2023-11-13 14:05:51 +0200

    Cheese

---
+++ Moduuli02/04/JS/Main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let numbers = []
+let number = 0
+do {
+        number = parseInt(prompt('Number here: '), 10)
+        numbers.push(number)
+} while(number != 0)
+
+numbers.pop(0)
+
+for (let index of numbers.reverse()){
+    console.log(index)
+}
