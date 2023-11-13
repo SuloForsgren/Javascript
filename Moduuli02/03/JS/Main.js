a2bc0807429caf2ba453fd259d93d14594b4b9e0
Your Name <you@example.com>
2023-11-13 13:37:58 +0200

    Cheese

--- Moduuli02/03/JS/Main.js
+++ Moduuli02/03/JS/Main.js
@@ -1,2 +1,11 @@
 'use strict'
 
+let dnames = []
+
+for (let i = 0; i < 6; i++){
+    let name = prompt('Dog name')
+    dnames.push(name)
+}
+
+for (let dname of dnames.reverse())
+document.getElementById('order').innerHTML += '<li>'+dname+'</li>'
