1c1d28eedaa6ef2c6d183f197e5cc904c06a2977
KoodiApina <sulonkoodit@gmail.com>
2023-11-23 21:40:52 +0200

    LilFixes

--- Moduuli03/module3-starters-main/t1/1.js
+++ Moduuli03/module3-starters-main/t1/1.js
@@ -1,10 +1,10 @@
 'use strict'
 
-let ul = document.querySelector('#target')
+const ul = document.getElementById('target')
 
 const item1 = '<li>"first item"</li>'
 const item2 = '<li>"Second item"</li>'
 const item3 = '<li>"Third item"</li>'
 
 ul.innerHTML = item1 + item2 + item3
-document.getElementById('target').classList.add("my-list")
+ul.classList.add("my-list")
