37c21ab046f4accef2dc11709c2a80199ebc9c00
KoodiApina <sulonkoodit@gmail.com>
2023-11-23 21:55:46 +0200

    Nothing

--- Moduuli03/module3-starters-main/t1/1.js
+++ Moduuli03/module3-starters-main/t1/1.js
@@ -2,9 +2,9 @@
 
 const ul = document.querySelector('#target')
 
-const item1 = '<li>"first item"</li>'
-const item2 = '<li>"Second item"</li>'
-const item3 = '<li>"Third item"</li>'
+const item1 = '<li>first item</li>'
+const item2 = '<li>Second item</li>'
+const item3 = '<li>Third item</li>'
 
 ul.innerHTML = item1 + item2 + item3
 document.querySelector("#target").classList.add("my-list");
