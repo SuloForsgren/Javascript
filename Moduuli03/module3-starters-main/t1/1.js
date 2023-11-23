d7c30aac1cb17f7e21ac1bdb1e8fad88b794dc68
KoodiApina <sulonkoodit@gmail.com>
2023-11-23 21:36:34 +0200

    Stupid?

--- Moduuli03/module3-starters-main/t1/1.js
+++ Moduuli03/module3-starters-main/t1/1.js
@@ -7,4 +7,4 @@ const item2 = '<li>"Second item"</li>'
 const item3 = '<li>"Third item"</li>'
 
 ul.innerHTML = item1 + item2 + item3
-document.querySelector('#target').classList.add("my-list")
+document.getElementById('target').classList.add("my-list")
