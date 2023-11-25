e8abe3d934a4504649c55b065a59c75ff47a0af5
KoodiApina <sulonkoodit@gmail.com>
2023-11-25 23:54:52 +0200

    All Done

--- Moduuli03/module3-starters-main/t10/10.js
+++ Moduuli03/module3-starters-main/t10/10.js
@@ -1,3 +1,13 @@
 'use strict'
-const names = document.querySelector('#source').value
-console.log(names)
+const form = document.querySelector('#source')
+const result = document.querySelector('#target')
+
+
+form.addEventListener('submit', function(button) {
+    button.preventDefault()
+    const fname = document.querySelector('input[name=firstname]').value
+    const lname = document.querySelector('input[name=lastname]').value
+
+    result.innerText = "Your name is " + fname + " " + lname
+    
+})
