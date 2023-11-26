ee30827a8a7cb2285e524adc8077be73999ab93f
KoodiApina <sulo.forsgren@gmail.com>
2023-11-26 18:34:54 +0200

    Teht05

---
+++ Moduuli04/05/Main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const getjoke = async() => {
+	const response = await fetch("https://api.chucknorris.io/jokes/random")
+	const data = await response.json()
+	console.log(data.value)
+}
+
+getjoke();
+
